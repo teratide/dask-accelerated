c339c58465a68ff52a688c022151bb1578657a1a
bobluppes <bobluppes@live.nl>
2021-03-31 01:46:38 +0200

    Extracted run functions to helper file

--- dask_accelerated/helpers.py
+++ dask_accelerated/helpers.py
@@ -1,4 +1,6 @@
 import dask
+import time
+from optimization import optimize_graph_re2
 
 # Returns a lazy result for the regex usecase in Dask
 def get_lazy_result(in_size):
@@ -27,3 +29,48 @@ def get_lazy_result(in_size):
 
     # Define the query using the pandas-like API
     return df[df["string"].str.match(regex)]["value"].sum()
+
+# Run the regex query on vanilla Dask
+def run_vanilla(size):
+
+    print("Running vanilla Dask on ", size, "M...\t", end="")
+
+    # Obtain the HighLevelGraph for the usecase
+    result = get_lazy_result(size)
+    graph = result.__dask_graph__()
+
+    # Convert the HighLevelGraph to a simpler task graph matching the Dask accelerated implementation
+    dsk = dict(graph)
+
+    # Perform the computations in the graph
+    start = time.time()
+    res = dask.get(dsk, (result.__dask_layers__()[0], 0))
+    end = time.time()
+
+    duration_in_seconds = end - start
+
+    print("Computed ", res, " in ", duration_in_seconds, " seconds")
+
+    return (res, duration_in_seconds)
+
+# Run the regex query on Dask + RE2
+def run_re2(size):
+    print("Running Dask + RE2 on ", size, "M...\t\t", end="")
+
+    # Obtain the HighLevelGraph for the usecase
+    result = get_lazy_result(size)
+    graph = result.__dask_graph__()
+
+    # Optimize the task graph by substituting the RE2 regex operator
+    dsk = optimize_graph_re2(graph, size)
+
+    # Perform the computations in the graph
+    start = time.time()
+    res = dask.get(dsk, (result.__dask_layers__()[0], 0))
+    end = time.time()
+
+    duration_in_seconds = end - start
+
+    print("Computed ", res, " in ", duration_in_seconds, " seconds")
+
+    return (res, duration_in_seconds)
--- dask_accelerated/main.py
+++ dask_accelerated/main.py
@@ -1,53 +1,11 @@
-import dask
-import time
-from helpers import get_lazy_result
-from optimization import optimize_graph_re2
+import helpers
 
-# Run the regex query on vanilla Dask
-def run_vanilla(size):
+def test_re2():
+    (vanilla_result, vanilla_duration) = helpers.run_vanilla(10)
+    (dask_result, dask_duration) = helpers.run_re2(10)
 
-    print("Running vanilla Dask on ", size, "M")
-
-    # Obtain the HighLevelGraph for the usecase
-    result = get_lazy_result(size)
-    graph = result.__dask_graph__()
-
-    # Convert the HighLevelGraph to a simpler task graph matching the Dask accelerated implementation
-    dsk = dict(graph)
-
-    # Perform the computations in the graph
-    start = time.time()
-    res = dask.get(dsk, (result.__dask_layers__()[0], 0))
-    end = time.time()
-
-    duration_in_seconds = end - start
-
-    return (res, duration_in_seconds)
-
-# Run the regex query on Dask + RE2
-def run_re2(size):
-    print("Running RE2 on ", size, "M")
-
-    # Obtain the HighLevelGraph for the usecase
-    result = get_lazy_result(size)
-    graph = result.__dask_graph__()
-
-    # Optimize the task graph by substituting the RE2 regex operator
-    dsk = optimize_graph_re2(graph, size)
-
-    # Perform the computations in the graph
-    start = time.time()
-    res = dask.get(dsk, (result.__dask_layers__()[0], 0))
-    end = time.time()
-
-    duration_in_seconds = end - start
-
-    return (res, duration_in_seconds)
+    assert vanilla_result == dask_result
 
 if __name__ == '__main__':
 
-    (result, duration_in_seconds) = run_vanilla(10)
-    print("Computed ", result, " in ", duration_in_seconds, " seconds\n")
-
-    (result, duration_in_seconds) = run_re2(10)
-    print("Computed ", result, " in ", duration_in_seconds, " seconds\n")
+    test_re2()
