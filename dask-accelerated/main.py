5aaf7d587e3818d12f8c39e0bbf8ee0aee4ce0cd
bobluppes <bobluppes@live.nl>
2021-03-30 15:08:45 +0200

    Main file now executes re2 implementation by default

--- dask-accelerated/main.py
+++ dask-accelerated/main.py
@@ -3,20 +3,43 @@ import time
 from helpers import get_lazy_result
 from optimization import optimize_graph
 
-def run_re2():
+def run_vanilla(size):
 
-    result = get_lazy_result(10)
+    print("Running vanilla Dask on ", size, "M")
+
+    result = get_lazy_result(size)
     graph = result.__dask_graph__()
 
-    dsk = optimize_graph(graph, 10)
-    #  dsk = dict(graph)
+    dsk = dict(graph)
 
     # Perform the computations in the graph
     start = time.time()
     res = dask.get(dsk, (result.__dask_layers__()[0], 0))
     end = time.time()
 
-    print("\n\nRESULT: ", res, "\tcomputed in ", end-start, " seconds")
+    duration_in_seconds = end - start
+
+    return (res, duration_in_seconds)
+
+
+def run_re2(size):
+    print("Running RE2 on ", size, "M")
+
+    result = get_lazy_result(size)
+    graph = result.__dask_graph__()
+
+    dsk = optimize_graph(graph, size)
+
+    # Perform the computations in the graph
+    start = time.time()
+    res = dask.get(dsk, (result.__dask_layers__()[0], 0))
+    end = time.time()
+
+    duration_in_seconds = end - start
+
+    return (res, duration_in_seconds)
 
 if __name__ == '__main__':
-    run_re2()
+    (res, duration_in_seconds) = run_re2(10)
+
+    print("Computed ", res, " in ", duration_in_seconds, " seconds")
--- dask-accelerated/operators.py
+++ dask-accelerated/operators.py
@@ -1,32 +1,34 @@
+import pyarrow
+import pandas
 from dask.dataframe.accessor import maybe_wrap_pandas
-# import dask_accelerated
+import dask_accelerated
 
 def custom_regex(obj, accessor, attr, args, kwargs):
-    # number_of_records = obj.size
-    # regex = args[0]
-    #
-    # arr = pyarrow.Array.from_pandas(obj)
-    # in_buffers = arr.buffers()
+    number_of_records = obj.size
+    regex = args[0]
+
+    arr = pyarrow.Array.from_pandas(obj)
+    in_buffers = arr.buffers()
 
     # Original computation
-    out = getattr(getattr(obj, accessor, obj), attr)(*args, **kwargs)
+    # out = getattr(getattr(obj, accessor, obj), attr)(*args, **kwargs)
 
-    # out = pandas.array([False] * number_of_records * 8, dtype=bool, copy=False)
-    # out_buffers = pyarrow.Array.from_pandas(original_out).buffers()
-    #
-    # dask_accelerated.re2Eval(
-    #     number_of_records,
-    #     regex,
-    #     in_buffers[1].address,
-    #     in_buffers[2].address,
-    #     in_buffers[1].size,
-    #     in_buffers[2].size,
-    #     out_buffers[1].address,
-    #     out_buffers[1].size
-    # )
-    #
-    # out = pyarrow.Array.from_buffers(pyarrow.bool_(), number_of_records, out_buffers).to_pandas()
-    #
+    out = pandas.array([False] * number_of_records * 8, dtype=bool, copy=False)
+    out_buffers = pyarrow.Array.from_pandas(out).buffers()
+
+    dask_accelerated.re2Eval(
+        number_of_records,
+        regex,
+        in_buffers[1].address,
+        in_buffers[2].address,
+        in_buffers[1].size,
+        in_buffers[2].size,
+        out_buffers[1].address,
+        out_buffers[1].size
+    )
+
+    out = pyarrow.Array.from_buffers(pyarrow.bool_(), number_of_records, out_buffers).to_pandas()
+    
     # orig = maybe_wrap_pandas(obj, original_out)
     # new = maybe_wrap_pandas(obj, out)
 
