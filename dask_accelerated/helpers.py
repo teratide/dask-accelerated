d7516fb176faf7959e9ad20a4478aafb738d8963
Bob Luppes <bobluppes@live.nl>
2021-03-31 16:57:04 +0200

    Automatically generate missing datasets

--- dask_accelerated/helpers.py
+++ dask_accelerated/helpers.py
@@ -1,27 +1,39 @@
 import dask
 import time
+import os
 import numpy as np
 from optimization import optimize_graph_re2
 from operators import CustomFilter
+from data_generator import gen
 
+
+# Convert to size string to match output of data generator
+def make_size_string(size):
+    size = int(size)
+    if (size >= 1e6):
+        size_str = str(int(size / 1e6)) + 'M'
+    else:
+        size_str = str(size)
+
+    return size_str
 
 # Returns a lazy result for the regex usecase in Dask
 def get_lazy_result(in_size):
 
     # In size is in millions of records
     # Append 'M' to match the dataset filenames
-    size = str(in_size) + "M"
+    size_str = make_size_string(in_size)
 
     # Constants
     chunksize = 1e6
     parquet_engine = "pyarrow"  # Valid engines: ['fastparquet', 'pyarrow', 'pyarrow-dataset', 'pyarrow-legacy']
-    file_root = "../data-generator/diving/data-"
+    file_root = "../data_generator/diving/data-"
     file_ext = ".parquet"
     regex = '.*[tT][eE][rR][aA][tT][iI][dD][eE][ \t\n]+[dD][iI][vV][iI][nN][gG][ \t\n]+([sS][uU][bB])+[sS][uU][rR][fF][aA][cC][eE].*'
 
     # Load the dataframe
     columns = ["value", "string"]
-    filename = file_root + size + file_ext
+    filename = file_root + size_str + file_ext
     df = dask.dataframe.read_parquet(
         filename,
         chunksize=chunksize,
@@ -52,7 +64,7 @@ def construct_durations(total, filter):
 # Run the regex query on vanilla Dask
 def run_vanilla(size):
 
-    print("Running vanilla Dask on ", size, "M...\t", end="")
+    print("Running vanilla Dask on ", size, "...\t", end="")
 
     # Obtain the HighLevelGraph for the usecase
     result = get_lazy_result(size)
@@ -81,7 +93,7 @@ def run_vanilla(size):
 
 # Run the regex query on Dask + RE2
 def run_re2(size):
-    print("Running Dask + RE2 on ", size, "M...\t\t", end="")
+    print("Running Dask + RE2 on ", size, "...\t\t", end="")
 
     # Obtain the HighLevelGraph for the usecase
     result = get_lazy_result(size)
@@ -106,3 +118,38 @@ def run_re2(size):
     durations = construct_durations(total_duration_in_seconds, filter_durations)
 
     return (res, durations)
+
+def generate_datasets_if_needed(sizes):
+    data_root = '../data_generator/diving'
+
+    missing_datasets = []
+    for size in sizes:
+
+        size_str = make_size_string(size)
+
+        file = '/data-' + size_str + '.parquet'
+
+        # Check if the dataset exists
+        if not os.path.isfile(data_root + file):
+            missing_datasets.append(size)
+
+    # Generate any missing datasets
+    if len(missing_datasets) > 0:
+        print("Missing datasets found, these will be generated")
+        match_percentage = 0.05
+        data_length = 100
+        regex = '.*[tT][eE][rR][aA][tT][iI][dD][eE][ \t\n]+[dD][iI][vV][iI][nN][gG][ \t\n]+([sS][uU][bB])+[sS][uU][rR][fF][aA][cC][eE].*'
+        parquet_chunksize = 1e6
+        parquet_compression = 'none'
+
+        gen.regex_strings(
+            missing_datasets,
+            match_percentage,
+            data_length,
+            regex,
+            parquet_chunksize,
+            parquet_compression,
+            data_root
+        )
+    else:
+        print("No missing datasets found")
--- dask_accelerated/main.py
+++ dask_accelerated/main.py
@@ -1,5 +1,5 @@
 import helpers
-import pickler
+import benchmarks
 
 def test_re2():
     (vanilla_result, vanilla_duration) = helpers.run_vanilla(10)
@@ -7,46 +7,11 @@ def test_re2():
 
     assert vanilla_result == dask_result
 
-def benchmark_filter_duration():
-    sizes = [10, 50, 100, 150]
-    repeats = 3
-
-    vanilla_filter = {}
-    re2_filter = {}
-    for size in sizes:
-        # Single run to mitigate caching effects
-        (res, dur) = helpers.run_vanilla(size)
-        (res, dur) = helpers.run_re2(size)
-
-        # Append 'M' to size and use as key to indicate 10e6 rows
-        key = str(size) + "M"
-        vanilla_filter[key] = 0
-        re2_filter[key] = 0
-
-        for i in range(repeats):
-            (res_vanilla, dur) = helpers.run_vanilla(size)
-            vanilla_filter[key] += dur['filter']['total']
-
-            (res_re2, dur) = helpers.run_re2(size)
-            re2_filter[key] += dur['filter']['total']
-
-            # Sanity check
-            assert res_vanilla == res_re2
-
-        # Average the duration
-        vanilla_filter[key] = vanilla_filter[key] / repeats
-        re2_filter[key] = re2_filter[key] / repeats
-
-    print(vanilla_filter)
-    print(re2_filter)
-
-    data = {
-        'vanilla_filter': vanilla_filter,
-        're2_filter': re2_filter
-    }
-
-    pickler.store_to_notebooks(data)
-
 if __name__ == '__main__':
 
-    benchmark_filter_duration()
+    sizes = [1e3, 64e3, 1024e3, 2048e3]
+    repeats = 3
+
+    helpers.generate_datasets_if_needed(sizes)
+
+    benchmarks.benchmark_filter_duration(sizes, repeats)
