0674b87c1e03c18011f57098dca8e2b11a805262
Bob Luppes <bobluppes@live.nl>
2021-03-31 21:13:44 +0200

    Added tidreEval operator

--- dask_accelerated/operators.py
+++ dask_accelerated/operators.py
@@ -59,3 +59,36 @@ class CustomFilter:
         self.durations.append(end-start)
 
         return maybe_wrap_pandas(obj, out)
+
+    # Performs string matching using Tidre on FPGA
+    def custom_tidre(self, obj, accessor, attr, args, kwargs):
+        # The number of records in the current batch
+        number_of_records = obj.size
+
+        # Extract arrow buffers from int input pandas series
+        arr = pyarrow.Array.from_pandas(obj)
+        in_buffers = arr.buffers()
+
+        # Initialize an empty selection vector and extract it's arrow buffers
+        out = pandas.array([False] * number_of_records, dtype=bool, copy=False)
+        out_buffers = pyarrow.Array.from_pandas(out).buffers()
+
+        # Do a native evaluation of the regex matching
+        start = time.time()
+        dask_native.tidreEval(
+            number_of_records,
+            in_buffers[1].address,
+            in_buffers[2].address,
+            in_buffers[1].size,
+            in_buffers[2].size,
+            out_buffers[1].address,
+            out_buffers[1].size
+        )
+        end = time.time()
+
+        # Reconstruct output selection vector from the underlying buffers
+        out = pyarrow.Array.from_buffers(pyarrow.bool_(), number_of_records, out_buffers).to_pandas()
+
+        self.durations.append(end - start)
+
+        return maybe_wrap_pandas(obj, out)
