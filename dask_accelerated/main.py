0609454f6d444d26f72aba92c29fef31095be59e
Bob Luppes <bobluppes@live.nl>
2021-03-31 13:54:41 +0200

    Added function to benchmark total filter duration

--- dask_accelerated/main.py
+++ dask_accelerated/main.py
@@ -6,6 +6,39 @@ def test_re2():
 
     assert vanilla_result == dask_result
 
+def benchmark_filter_duration():
+    sizes = [10, 50, 100, 150]
+    repeats = 3
+
+    vanilla_filter = {}
+    re2_filter = {}
+    for size in sizes:
+        # Single run to mitigate caching effects
+        (res, dur) = helpers.run_vanilla(size)
+        (res, dur) = helpers.run_re2(size)
+
+        # Append 'M' to size and use as key to indicate 10e6 rows
+        key = str(size) + "M"
+        vanilla_filter[key] = 0
+        re2_filter[key] = 0
+
+        for i in range(repeats):
+            (res_vanilla, dur) = helpers.run_vanilla(size)
+            vanilla_filter[key] += dur['filter']['total']
+
+            (res_re2, dur) = helpers.run_re2(size)
+            re2_filter[key] += dur['filter']['total']
+
+            # Sanity check
+            assert res_vanilla == res_re2
+
+        # Average the duration
+        vanilla_filter[key] = vanilla_filter[key] / repeats
+        re2_filter[key] = re2_filter[key] / repeats
+
+    print(vanilla_filter)
+    print(re2_filter)
+
 if __name__ == '__main__':
 
-    test_re2()
+    benchmark_filter_duration()
