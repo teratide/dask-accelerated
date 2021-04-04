e4dd4320cf3b5584ba410bc96e246cb78121d724
bobluppes <bobluppes@live.nl>
2021-04-04 17:25:27 +0200

    Changed default benchmark in main.py to tidre

--- dask_accelerated/main.py
+++ dask_accelerated/main.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     batch_sizes = [1e3, 2e3, 4e3, 8e3, 16e3, 32e3, 64e3, 128e3, 256e3, 512e3, 1024e3, 2048e3]
     repeats = 3
     helpers.generate_datasets_if_needed(in_sizes)
-    benchmarks.benchmark_re2(in_sizes, batch_sizes, repeats)
+    benchmarks.benchmark_tidre(in_sizes, batch_sizes, repeats)
