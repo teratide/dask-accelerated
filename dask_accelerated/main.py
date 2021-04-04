4933511e2124f24e5219b6483e7f2e4c91c08cb8
bobluppes <bobluppes@live.nl>
2021-04-04 17:26:00 +0200

    Removed redundant tests from main.py

--- dask_accelerated/main.py
+++ dask_accelerated/main.py
@@ -2,20 +2,6 @@ import helpers
 import benchmarks
 
 
-def test_re2(size):
-    (re2_result, re2_duration) = helpers.run_re2(size)
-    (vanilla_result, vanilla_duration) = helpers.run_vanilla(size)
-
-    assert vanilla_result == re2_result
-
-
-def test_tidre(size):
-    (tidre_result, tidre_duration) = helpers.run_tidre(size)
-    (vanilla_result, vanilla_duration) = helpers.run_vanilla(size)
-
-    assert vanilla_result == tidre_result
-
-
 if __name__ == '__main__':
 
     in_sizes = [1e3, 2e3, 4e3, 8e3, 16e3, 32e3, 64e3, 128e3, 256e3, 512e3, 1024e3, 2048e3]
