f159ccc1caa4484be294e8ce35a776443662749a
bobluppes <bobluppes@live.nl>
2021-04-05 23:21:34 +0200

    Added command line parameters to dask accelerated

--- dask_accelerated/main.py
+++ dask_accelerated/main.py
@@ -1,8 +1,49 @@
-from benchmark import benchmarks
+import argparse
+import helpers
+
+
+# Custom conversion to support strings of '64e3' to int
+def scientific_to_int(val):
+    return int(float(val))
+
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Dask Accelerated.')
+parser.add_argument('in_size', metavar='N', type=scientific_to_int,
+                    help='input size in records at which to run dask accelerated')
+parser.add_argument('batch_size', metavar='B', type=scientific_to_int,
+                    help='batch size in records at which to run dask accelerated, must be a multiple of 1000')
+parser.add_argument('--vanilla', dest='vanilla', action='store_const',
+                    const=True, default=False,
+                    help='run vanilla Dask. (default: do not run)')
+parser.add_argument('--re2', dest='re2', action='store_const',
+                    const=True, default=False,
+                    help='run the RE2 implementation. (default: do not run)')
+parser.add_argument('--tidre', dest='tidre', action='store_const',
+                    const=True, default=False,
+                    help='run the Tidre implementation. (default: do not run)')
+
+args = parser.parse_args()
+
 
 if __name__ == '__main__':
 
-    in_sizes = [1e3, 2e3, 4e3, 8e3, 16e3, 32e3, 64e3, 128e3, 256e3, 512e3, 1024e3, 2048e3]
-    batch_aggregates = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
-    repeats = 3
-    benchmarks.benchmark_tidre(in_sizes, batch_aggregates, repeats)
+    in_size = args.in_size
+    batch_size = args.batch_size
+
+    # The actual batch size in computed by aggregating
+    # multiple chunks of 1e3 records into the desired batch size
+    batch_aggregate = int(batch_size / 1e3)
+    batch_size = 1e3
+
+    # Make sure the desired dataset exists
+    helpers.generate_datasets_if_needed([in_size], batch_size)
+
+    if args.vanilla:
+        helpers.run_vanilla(in_size, batch_size, batch_aggregate)
+
+    if args.re2:
+        helpers.run_re2(in_size, batch_size, batch_aggregate)
+
+    if args.tidre:
+        helpers.run_tidre(in_size, batch_size, batch_aggregate)
