65becd9fb5d3f5b40fd6d9656b54366951490d2d
bobluppes <bobluppes@live.nl>
2021-04-05 22:10:51 +0200

    Added command line argument enable tidre benchmarks

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,7 +1,16 @@
+import argparse
 import benchmarks as bench
 import helpers as benchmark_helpers
 from dask_accelerated import helpers
 
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Benchmark the RE2 and Tidre implementations of dask_accelerated.')
+parser.add_argument('--tidre', dest='tidre', action='store_const',
+                    const=True, default=False,
+                    help='run the Tidre benchmarks. (default: only run RE2)')
+
+args = parser.parse_args()
 
 def benchmark_re2(in_sizes, batch_aggregates, repeats):
 
@@ -52,4 +61,8 @@ if __name__ == '__main__':
     in_sizes = [1e3, 2e3, 4e3, 8e3, 16e3, 32e3, 64e3, 128e3, 256e3, 512e3, 1024e3, 2048e3]
     batch_aggregates = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
     repeats = 3
+
     benchmark_re2(in_sizes, batch_aggregates, repeats)
+    if args.tidre:
+        benchmark_tidre(in_sizes, batch_aggregates, repeats)
+
