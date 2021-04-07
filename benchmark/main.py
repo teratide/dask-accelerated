531708dfa73b9e18b9325b926039920fd06fd619
Bob Luppes <bobluppes@live.nl>
2021-04-07 13:37:51 +0200

    Made benchmarks bigger, in_size benchmark runs up to 4M records and batch_size runs on 4M with batch sizes up to 8M

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 import benchmarks as bench
 import helpers as benchmark_helpers
 from dask_accelerated import helpers
@@ -15,13 +16,15 @@ args = parser.parse_args()
 def benchmark_re2(in_sizes, batch_aggregates, repeats):
 
     # Constants when varying a single parameter
-    constant_in_size = 1e6
+    constant_in_size = 4e6
     constant_batch_size_in_benchmark = 1e6
+
+    # Don't change these
     constant_batch_size_batch_benchmark = 1e3
     constant_batch_aggregate = 1
 
     helpers.generate_datasets_if_needed(in_sizes, constant_batch_size_in_benchmark)
-    helpers.generate_datasets_if_needed(in_sizes, constant_batch_size_batch_benchmark)
+    helpers.generate_datasets_if_needed([constant_in_size], constant_batch_size_batch_benchmark)
 
     data = {}
 
@@ -37,13 +40,15 @@ def benchmark_re2(in_sizes, batch_aggregates, repeats):
 def benchmark_tidre(in_sizes, batch_aggregates, repeats):
 
     # Constants when varying a single parameter
-    constant_in_size = 1e6
+    constant_in_size = 4e6
     constant_batch_size_in_benchmark = 1e6
+
+    # Don't change these
     constant_batch_size_batch_benchmark = 1e3
     constant_batch_aggregate = 1
 
     helpers.generate_datasets_if_needed(in_sizes, constant_batch_size_in_benchmark)
-    helpers.generate_datasets_if_needed(in_sizes, constant_batch_size_batch_benchmark)
+    helpers.generate_datasets_if_needed([constant_in_size], constant_batch_size_batch_benchmark)
 
     data = {}
 
@@ -58,12 +63,16 @@ def benchmark_tidre(in_sizes, batch_aggregates, repeats):
 
 if __name__ == '__main__':
 
-    in_sizes = [1e3, 2e3, 4e3, 8e3, 16e3, 32e3, 64e3, 128e3, 256e3, 512e3, 1024e3, 2048e3]
-    batch_aggregates = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+    in_sizes = [1e3, 2e3, 4e3, 8e3, 16e3, 32e3, 64e3, 128e3, 256e3, 512e3, 1024e3, 2048e3, 4096e3]
+    batch_aggregates = [64, 128, 256, 512, 1024, 2048, 4096, 8192]
     repeats = 3
 
+    start = time.time()
     if args.tidre:
         benchmark_tidre(in_sizes, batch_aggregates, repeats)
     else:
         benchmark_re2(in_sizes, batch_aggregates, repeats)
+    end = time.time()
+
+    print("Ran all benchmarks in ", (end - start) / 60, " minutes")
 
