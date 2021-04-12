0e626105291cd47369b6f700ec0fb779c3c08867
Bob Luppes <bobluppes@live.nl>
2021-04-12 12:04:50 +0200

    Accept repeats as command line parameter

--- dask_accelerated/main.py
+++ dask_accelerated/main.py
@@ -13,6 +13,8 @@ parser.add_argument('in_size', metavar='N', type=scientific_to_int,
                     help='input size in records at which to run dask accelerated')
 parser.add_argument('batch_size', metavar='B', type=scientific_to_int,
                     help='batch size in records at which to run dask accelerated, must be a multiple of 1000')
+parser.add_argument('repeats', metavar='R', type=scientific_to_int,
+                    help='the number of times to repeat the experiment')
 parser.add_argument('--vanilla', dest='vanilla', action='store_const',
                     const=True, default=False,
                     help='run vanilla Dask. (default: do not run)')
@@ -30,6 +32,7 @@ if __name__ == '__main__':
 
     in_size = args.in_size
     batch_size = args.batch_size
+    repeats = args.repeats
 
     # The actual batch size in computed by aggregating
     # multiple chunks of 1e3 records into the desired batch size
@@ -40,10 +43,25 @@ if __name__ == '__main__':
     helpers.generate_datasets_if_needed([in_size], batch_size)
 
     if args.vanilla:
-        helpers.run_vanilla(in_size, batch_size, batch_aggregate)
+        filter_duration = 0
+        for i in range(repeats):
+            (res, dur) = helpers.run_vanilla(in_size, batch_size, batch_aggregate)
+            filter_duration += dur['filter']['total']
+        filter_duration = filter_duration / repeats
+        print("Ran vanilla ", repeats, " times, ave total filter: ", filter_duration, " seconds")
 
     if args.re2:
-        helpers.run_re2(in_size, batch_size, batch_aggregate)
+        filter_duration = 0
+        for i in range(repeats):
+            (res, dur) = helpers.run_re2(in_size, batch_size, batch_aggregate)
+            filter_duration += dur['filter']['total']
+        filter_duration = filter_duration / repeats
+        print("Ran RE2 ", repeats, " times, ave total filter: ", filter_duration, " seconds")
 
     if args.tidre:
-        helpers.run_tidre(in_size, batch_size, batch_aggregate)
+        filter_duration = 0
+        for i in range(repeats):
+            (res, dur) = helpers.run_tidre(in_size, batch_size, batch_aggregate)
+            filter_duration += dur['filter']['total']
+        filter_duration = filter_duration / repeats
+        print("Ran Tidre ", repeats, " times, ave total filter: ", filter_duration, " seconds")
