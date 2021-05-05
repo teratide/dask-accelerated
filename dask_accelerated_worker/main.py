92788800c92c59ffa87d5afd0601b856d2e1c092
Bob Luppes <bobluppes@live.nl>
2021-05-05 17:50:21 +0200

    Added batch size benchmark

--- dask_accelerated_worker/main.py
+++ dask_accelerated_worker/main.py
@@ -34,10 +34,12 @@ def main():
 
     input("Press Enter to perform benchmarks...")
 
-    in_sizes = [1e3, 2e3, 4e3, 8e3, 16e3, 32e3, 64e3, 128e3, 256e3, 512e3, 1024e3, 2048e3, 4096e3]
+    const_in_size = 4096e3
+    in_sizes = [512e3, 1024e3, 2048e3, 4096e3, 8192e3]
     batch_size = 1e3
-    batch_aggregate = 16
-    repeats = 3
+    const_batch_aggregate = 1e3
+    batch_aggregates = [64, 128, 256, 512, 1024, 2048, 4096, 8192]
+    repeats = 5
 
     # Make sure the desired dataset exists
     helpers.generate_datasets_if_needed(in_sizes, batch_size)
@@ -45,13 +47,19 @@ def main():
     # Keep running the benchmark until the user quits the client
     while True:
         data = {}
+        data_in_size = {}
+        data_batch_size = {}
 
+        # In size benchmark
         for in_size in in_sizes:
 
-            lazy_result = helpers.get_lazy_result(in_size, batch_size, batch_aggregate)
+            lazy_result = helpers.get_lazy_result(in_size, batch_size, const_batch_aggregate)
             graph = lazy_result.__dask_graph__()
 
-            data[in_size] = 0
+            # Dry run
+            res = client.get(graph, (lazy_result.__dask_layers__()[0], 0))
+
+            data_in_size[in_size] = 0
 
             for i in range(repeats):
 
@@ -60,23 +68,53 @@ def main():
                 end = time.time()
 
                 duration = end - start
-                data[in_size] += duration
-                print('Computed ', res, ' in ', duration, ' seconds')
+                data_in_size[in_size] += duration
+                print('In: ', in_size, '\tBatch: ', batch_size * const_batch_aggregate, '\tComputed ', res, ' in ', duration, ' seconds')
 
-            data[in_size] = data[in_size] / repeats
+            data_in_size[in_size] = data_in_size[in_size] / repeats
 
-        workers = scheduler.workers
+        # Batch size benchmark
+        for batch_aggregate in batch_aggregates:
 
-        timestamp = datetime.now().strftime("%d-%b-%Y_%H:%M:%S")
+            lazy_result = helpers.get_lazy_result(const_in_size, batch_size, batch_aggregate)
+            graph = lazy_result.__dask_graph__()
+
+            # Dry run
+            res = client.get(graph, (lazy_result.__dask_layers__()[0], 0))
+
+            data_batch_size[batch_aggregate] = 0
+
+            for i in range(repeats):
+                start = time.time()
+                res = client.get(graph, (lazy_result.__dask_layers__()[0], 0))
+                end = time.time()
+
+                duration = end - start
+                data_batch_size[batch_aggregate] += duration
+                print('In: ', const_in_size, '\tBatch: ', batch_size * batch_aggregate, 'Computed ', res, ' in ', duration, ' seconds')
+
+            data_batch_size[batch_aggregate] = data_batch_size[batch_aggregate] / repeats
+
+        # Count the number of accelerated workers
+        accelerated_workers = 0
+        for worker in scheduler.workers:
+            if str(workers[worker].name).split('-')[0] == 'accelerated':
+                accelerated_workers += 1
+
+        data[str(accelerated_workers)]['in_size'] = data_in_size
+        data[str(accelerated_workers)]['batch_size'] = data_batch_size
+
         # Add timestamp to data
+        timestamp = datetime.now().strftime("%d-%b-%Y_%H:%M:%S")
         data['timestamp'] = timestamp
-        data_root = '../notebooks/'
-        with open(data_root + 'data-' + str(len(workers)) + '.pickle', 'wb') as f:
-            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
         user_input = input("Press Enter to run again or send 'q' to close the client...")
         if user_input == 'q':
             break
+
+    data_root = '../notebooks/'
+    with open(data_root + 'data-workers.pickle', 'wb') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
     client.close()
 
